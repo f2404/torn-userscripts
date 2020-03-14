274de1352b7c7f8cdd88ec1c492743a851175967
Igor <f2404@yandex.ru>
2020-03-13 21:48:37 -0400

    Add option to hide loaned items

--- faction_filter_armory_weapons.user.js
+++ faction_filter_armory_weapons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn: Faction: Filter armory weapons
 // @namespace    lugburz.faction.filter_armory_weapons
-// @version      0.3.1
+// @version      0.4
 // @description  Filter weapons and armor by type in faction armory.
 // @author       Lugburz
 // @match        https://www.torn.com/factions.php?step=your*
@@ -24,15 +24,22 @@ function showHideItems(checked, cb_name, cb_id) {
             if (cb_name.localeCompare('Body') == 0) {
                 // there're a lot of names for body armor...
                 matches = (typeof name !== 'undefined' && name !== null &&
-                           ($(name).text().includes('Vest') || $(name).text().includes('Jacket') || $(name).text().includes('Mail') || $(name).text().includes('Armor') || $(name).text().includes('Armour')));
+                           ($(name).text().includes('Vest') || $(name).text().includes('Jacket') || $(name).text().includes('Mail') ||
+                            $(name).text().includes('Armor') || $(name).text().includes('Armour')));
             } else {
                 matches = (typeof name !== 'undefined' && name !== null && $(name).text().includes(cb_name));
             }
         }
 
+        // nhidden shows how many times an item has been hidden
+        let nhidden = $(this).attr('nhidden');
+        if (typeof nhidden === 'undefined' || nhidden === null) nhidden = 0;
+
         if (matches && checked) {
-            $(this).show();
+            if (nhidden > 0) $(this).attr('nhidden', Number(nhidden) - Number(1));
+            if ($(this).attr('nhidden') < 1) $(this).show();
         } else if (matches) {
+            $(this).attr('nhidden', Number(nhidden) + Number(1));
             $(this).hide();
         }
     });
@@ -43,14 +50,14 @@ function addWeaponDivs() {
         return;
     }
 
-    let types = ['primary', 'secondary', 'melee'];
+    let types = ['primary', 'secondary', 'melee', 'show-loaned'];
     let id_pref = 'weapon-';
 
     let primCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-primary" name="Primary"><label for="Primary">Primary</label>';
     let secondCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-secondary" name="Secondary"><label for="Secondary">Secondary</label>';
     let meleeCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-melee" name="Melee"><label for="Melee">Melee</label>';
     let showLoanedCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-show-loaned" name="Show-loaned"><label for="Show-loaned">Show loaned</label>';
-    let myDiv = '<div class="title-black top-round t-overflow"><span>Show weapon types:</span>' + primCB + secondCB + meleeCB + '</div';
+    let myDiv = '<div class="title-black top-round t-overflow"><span>Show weapon types:</span>' + primCB + secondCB + meleeCB + showLoanedCB + '</div';
     $("#armoury-weapons").prepend(myDiv);
 
     types.forEach(function(item, index, array) {
@@ -76,7 +83,7 @@ function addArmorDivs() {
         return;
     }
 
-    let types = ['boots', 'gloves', 'helmet', 'pants', 'body'];
+    let types = ['boots', 'gloves', 'helmet', 'pants', 'body', 'show-loaned'];
     let id_pref = 'armor-';
 
     let bootsCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="armor-boots" name="Boots"><label for="Boots">Boots</label>';
@@ -85,7 +92,7 @@ function addArmorDivs() {
     let pantsCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="armor-pants" name="Pants"><label for="Pants">Pants</label>';
     let bodyCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="armor-body" name="Body"><label for="Body">Body</label>';
     let showLoanedCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="armor-show-loaned" name="Show-loaned"><label for="Show-loaned">Show loaned</label>';
-    let myDiv = '<div class="title-black top-round t-overflow"><span>Show armor types:</span>' + bootsCB + glovesCB + helmetCB + pantsCB + bodyCB + '</div';
+    let myDiv = '<div class="title-black top-round t-overflow"><span>Show armor types:</span>' + bootsCB + glovesCB + helmetCB + pantsCB + bodyCB + showLoanedCB + '</div';
     $("#armoury-armour").prepend(myDiv);
 
     types.forEach(function(item, index, array) {
@@ -122,3 +129,4 @@ function addArmorDivs() {
     $("#armoury-weapons").ready(addWeaponDivs);
     $("#armoury-armour").ready(addArmorDivs);
 })();
+
