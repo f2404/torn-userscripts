2c0b5ece269c7eb2b80f37769f08c07c075ea467
Igor <f2404@yandex.ru>
2020-03-16 17:15:23 -0400

    Add stocks sorting

--- sort_lists.user.js
+++ sort_lists.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Torn: Sort lists
 // @namespace    lugburz.sort_lists
-// @version      0.2
+// @version      0.3
 // @description  Sort lists (such as blacklist, friendlist, faction members list) by various columns.
 // @author       Lugburz
 // @match        https://www.torn.com/blacklist.php*
 // @match        https://www.torn.com/friendlist.php*
 // @match        https://www.torn.com/factions.php*
+// @match        https://www.torn.com/stockexchange.php*
 // @require      https://greasyfork.org/scripts/390917-dkk-torn-utilities/code/DKK%20Torn%20Utilities.js?version=744690
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -43,17 +44,17 @@ function compare (aText, bText, asc) {
     return 0;
 }
 
-function doSort(users, column, ascending) {
+function doSort(items, column, ascending, divPrefix = '.title-black > .') {
     if ('level'.localeCompare(column) == 0 || 'lvl'.localeCompare(column) == 0 || 'days'.localeCompare(column) == 0) {
-        let sortedByLevel = Array.prototype.sort.bind(users);
+        let sortedByLevel = Array.prototype.sort.bind(items);
         sortedByLevel(function (a, b) {
             let aText = $(a).find('.'+column).text().match(/(\d+)/)[0];
             let bText = $(b).find('.'+column).text().match(/(\d+)/)[0];
 
             return compare(Number(aText), Number(bText), ascending);
         });
-    } else if ('title'.localeCompare(column) == 0 || 'desk'.localeCompare(column) == 0) {
-        let sortedByName = Array.prototype.sort.bind(users);
+    } else if ('title'.localeCompare(column) == 0 || 'desk'.localeCompare(column) == 0 || 'name'.localeCompare(column) == 0) {
+        let sortedByName = Array.prototype.sort.bind(items);
         sortedByName(function (a, b) {
             // works with honors enabled or disabled
             let aText = $(a).find('.name').attr('data-placeholder') || $(a).find('.name').text();
@@ -64,7 +65,7 @@ function doSort(users, column, ascending) {
             return compare(aText, bText, ascending);
         });
     } else if ('status'.localeCompare(column) == 0) {
-        let sortedByStatus = Array.prototype.sort.bind(users);
+        let sortedByStatus = Array.prototype.sort.bind(items);
         sortedByStatus(function (a, b) {
             let aText = $(a).find('.'+column).text().replace('Status:', '').trim();
             let bText = $(b).find('.'+column).text().replace('Status:', '').trim();
@@ -75,9 +76,9 @@ function doSort(users, column, ascending) {
     } else if ('member-icons'.localeCompare(column) == 0) {
         // Do not show arrows if no 'Last Action'
         if ($('.title-black > .'+column).text() === 'undefined' || !$('.title-black > .'+column).text().includes('Last Action'))
-            return users;
+            return items;
 
-        let sortedByLastAction = Array.prototype.sort.bind(users);
+        let sortedByLastAction = Array.prototype.sort.bind(items);
         sortedByLastAction(function (a, b) {
             let aText = $(a).find('.last-action').text().trim();
             let bText = $(b).find('.last-action').text().trim();
@@ -94,13 +95,26 @@ function doSort(users, column, ascending) {
 
             return compare (Number(aMins), Number(bMins), ascending);
         });
+    } else if ('price'.localeCompare(column) == 0 || 'owned'.localeCompare(column) == 0 || 'change'.localeCompare(column) == 0) {
+        let sortedByPrice = Array.prototype.sort.bind(items);
+        sortedByPrice(function (a, b) {
+            let aText = $(a).find('.'+column).text().match(/((\d+\,)?\d+(\.\d+)?)/)[0].replace(',', '');
+            let bText = $(b).find('.'+column).text().match(/((\d+\,)?\d+(\.\d+)?)/)[0].replace(',', '');
+
+            // 'down' means the number is negative
+            if ('change'.localeCompare(column) == 0) {
+                if ($(a).find('.'+column).hasClass('down')) aText = -1 * Number(aText);
+                if ($(b).find('.'+column).hasClass('down')) bText = -1 * Number(bText);
+            }
+
+            return compare(Number(aText), Number(bText), ascending);
+        });
     } else {
         // shouldn't happen
-        return users;
+        return items;
     }
 
-    let divPrefix = '.title-black > .';
-    let columns = [ 'level', 'lvl', 'title', 'desk', 'days', 'status', 'member-icons'].forEach((elem) => {
+    let columns = [ 'level', 'lvl', 'title', 'desk', 'name', 'days', 'status', 'member-icons', 'price', 'owned', 'change'].forEach((elem) => {
         $(divPrefix+elem).removeClass('headerSortUp');
         $(divPrefix+elem).removeClass('headerSortDown');
     });
@@ -111,7 +125,7 @@ function doSort(users, column, ascending) {
         $(divPrefix+column).addClass('headerSortUp');
     }
 
-    return users;
+    return items;
 }
 
 // Blacklist, friendlist
@@ -152,6 +166,25 @@ function addMemberlistSort() {
     });
 }
 
+// Stocks
+function addStocklistSort() {
+    let stock_list = $('ul.stock-list');
+    let stocks = $(stock_list).children('li');
+    let ascending = true;
+    let last_sort = '';
+
+    let columns = ['name', 'price', 'change', 'owned'].forEach((column) => {
+        $('div.title-black > ul.title > li.'+column).addClass('headerSortable');
+        $('div.title-black > ul.title > li.'+column).on('click', function() {
+            if (column != last_sort) ascending = true;
+            last_sort = column;
+            stocks = doSort(stocks, column, ascending, 'div.title-black > ul.title > li.');
+            ascending = !ascending;
+            $(stock_list).append(stocks);
+        });
+    });
+}
+
 (function() {
     'use strict';
 
@@ -161,11 +194,15 @@ function addMemberlistSort() {
             $('ul.user-info-blacklist-wrap').ready(addUserlistSort);
         } else if (page == "factions") {
             $('ul.member-list').ready(addMemberlistSort);
+        } else if (page == "stockexchange") {
+            $('ul.stock-list').ready(addStocklistSort);
         }
     });
 
     //$('ul.container-body-list').ready(a);
     if ($(location).attr('href').includes('factions.php')) {
         $('ul.member-list').ready(addMemberlistSort);
+    } else if ($(location).attr('href').includes('stockexchange.php')) {
+        $('ul.stock-list').ready(addStocklistSort);
     }
 })();
