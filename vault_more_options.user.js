15f1ff0d7d92230134eb0644ad3ff794b88f7b0d
Igor <f2404@yandex.ru>
2020-03-24 20:16:16 -0400

    React to button style changes

--- vault_more_options.user.js
+++ vault_more_options.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn: Vault: More options
 // @namespace    lugburz.vault.more_options
-// @version      0.1.1
+// @version      0.1.2
 // @description  Add 'Upkeep' button that automatically inputs the amount needed to pay upkeep.
 // @author       Lugburz
 // @match        https://www.torn.com/properties.php*
@@ -20,7 +20,7 @@ function addRentButton() {
     }
 
     if (document.getElementById(btnId) == null) {
-        $('div.vault-wrap > form.vault-cont.left > div.cont').append('<span id="' + btnId + '" class="btn-wrap silver"><span class="btn"><button class="wai-btn button-btn">Upkeep</button></span></span>');
+        $('div.vault-wrap > form.vault-cont.left > div.cont').append('<span id="' + btnId + '" class="btn-wrap silver"><span class="btn"><button class="wai-btn button-btn" title="Upkeep">$</button></span></span>');
     }
 
     if (upkeep < 1) {
