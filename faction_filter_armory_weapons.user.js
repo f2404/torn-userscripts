43fcd17f1bb387f92bd21bcae3aafcaeb595f6a8
Igor <f2404@yandex.ru>
2020-03-29 16:45:03 -0400

    Refactoring

--- faction_filter_armory_weapons.user.js
+++ faction_filter_armory_weapons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn: Faction: Filter armory weapons
 // @namespace    lugburz.faction.filter_armory_weapons
-// @version      0.4.1
+// @version      0.4.2
 // @description  Filter weapons and armor by type in faction armory.
 // @author       Lugburz
 // @match        https://www.torn.com/factions.php?step=your*
@@ -11,16 +11,16 @@
 // ==/UserScript==
 
 function showHideItems(checked, cb_name, cb_id) {
-    $(".item-list > li").each(function(index) {
+    $('.item-list > li').each(function(index) {
         let matches;
         if (cb_name.localeCompare('Show-loaned') == 0) {
-            let loaned = $(this).find(".loaned");
+            let loaned = $(this).find('.loaned');
             matches = (typeof loaned !== 'undefined' && loaned !== null && !$(loaned).text().includes('Available'));
         } else if (cb_id.startsWith('weapon-')) {
-            let type = $(this).find(".type");
+            let type = $(this).find('.type');
             matches = (typeof type !== 'undefined' && type !== null && cb_name.localeCompare($(type).text()) == 0);
         } else if (cb_id.startsWith('armor-')) {
-            let name = $(this).find(".name");
+            let name = $(this).find('.name');
             if (cb_name.localeCompare('Body') == 0) {
                 // there're a lot of names for body armor...
                 matches = (typeof name !== 'undefined' && name !== null &&
@@ -45,22 +45,8 @@ function showHideItems(checked, cb_name, cb_id) {
     });
 }
 
-function addWeaponDivs() {
-    if (!$(location).attr('href').includes('sub=weapons')) {
-        return;
-    }
-
-    let types = ['primary', 'secondary', 'melee', 'show-loaned'];
-    let id_pref = 'weapon-';
-
-    let primCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-primary" name="Primary"><label for="Primary">Primary</label>';
-    let secondCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-secondary" name="Secondary"><label for="Secondary">Secondary</label>';
-    let meleeCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-melee" name="Melee"><label for="Melee">Melee</label>';
-    let showLoanedCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-show-loaned" name="Show-loaned"><label for="Show-loaned">Show loaned</label>';
-    let myDiv = '<div class="title-black top-round t-overflow"><span>Show weapon types:</span>' + primCB + secondCB + meleeCB + showLoanedCB + '</div';
-    $("#armoury-weapons").prepend(myDiv);
-
-    types.forEach(function(item, index, array) {
+function processTypes(types, id_pref) {
+    types.forEach(function(item) {
         let id = id_pref + item;
 
         if (GM_getValue(id) == "0") {
@@ -76,6 +62,24 @@ function addWeaponDivs() {
             showHideItems(this.checked, $(this).attr("name"), id);
         });
     });
+}
+
+function addWeaponDivs() {
+    if (!$(location).attr('href').includes('sub=weapons')) {
+        return;
+    }
+
+    let types = ['primary', 'secondary', 'melee', 'show-loaned'];
+    let id_pref = 'weapon-';
+
+    let primCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-primary" name="Primary"><label for="Primary">Primary</label>';
+    let secondCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-secondary" name="Secondary"><label for="Secondary">Secondary</label>';
+    let meleeCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-melee" name="Melee"><label for="Melee">Melee</label>';
+    let showLoanedCB = '<input type="checkbox" style="margin-left: 5px; margin-right: 5px" id="weapon-show-loaned" name="Show-loaned"><label for="Show-loaned">Show loaned</label>';
+    let myDiv = '<div class="title-black top-round t-overflow"><span>Show weapon types:</span>' + primCB + secondCB + meleeCB + showLoanedCB + '</div';
+    $("#armoury-weapons").prepend(myDiv);
+
+    processTypes(types, id_pref);
 }
 
 function addArmorDivs() {
@@ -95,22 +99,7 @@ function addArmorDivs() {
     let myDiv = '<div class="title-black top-round t-overflow"><span>Show armor types:</span>' + bootsCB + glovesCB + helmetCB + pantsCB + bodyCB + showLoanedCB + '</div';
     $("#armoury-armour").prepend(myDiv);
 
-    types.forEach(function(item, index, array) {
-        let id = id_pref + item;
-
-        if (GM_getValue(id) == "0") {
-            $("#"+id).prop("checked", false);
-        } else {
-            $("#"+id).prop("checked", true);
-        }
-
-        showHideItems($("#"+id).prop("checked"), $("#"+id).attr("name"), id);
-
-        $("#"+id).change(function() {
-            GM_setValue(id, this.checked);
-            showHideItems(this.checked, $(this).attr("name"), id);
-        });
-    });
+    processTypes(types, id_pref);
 };
 
 (function() {
