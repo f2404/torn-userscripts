8d0a9a8e49f2344649c58eec510ec8bf3637f936
Igor <f2404@yandex.ru>
2020-04-02 10:55:52 -0400

    Add support for Global, Faction, Travel chats

--- chat_filter.user.js
+++ chat_filter.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Chat: Filter
 // @namespace    lugburz.filter_chat
-// @version      0.1
-// @description  Add filtering by keyword to chats (Trade chat atm).
+// @version      0.2
+// @description  Add filtering by keyword to chats (Global, Trade, Faction, Travel).
 // @author       Lugburz
 // @match        https://www.torn.com/*
 // @grant        GM_setValue
@@ -10,9 +10,9 @@
 // ==/UserScript==
 
 function filter(content, keyword) {
-    let msgs = $(content).find('div[class^=message_]');
+    const msgs = $(content).find('div[class^=message_]');
     $(msgs).each(function() {
-        let msg = $(this).text();
+        const msg = $(this).text();
         if (msg.toLowerCase().includes(keyword.toLowerCase()))
             $(this).show();
         else
@@ -20,38 +20,44 @@ function filter(content, keyword) {
     });
 }
 
-function addTradeFilter(box) {
-    let filter_name = 'filter-trade';
-    let title = $(box).find('div[class^=chat-box-title_]');
-    if (title.size() > 0 && $(title).attr('title') == 'Trade') {
-        if ($(box).find('#'+filter_name).size() > 0)
-            return;
+function addChatFilter(box, chat) {
+    const content = $(box).find('div[class^=chat-box-content_]');
+    const filter_name = 'filter-' + chat;
 
-        let input = $(box).find('div[class^=chat-box-input_]');
-        let content = $(box).find('div[class^=chat-box-content_]');
-        $(input).prepend('<div><span style="vertical-align: middle; padding: 10px;"><label for="filter" style="color: green;">Filter: </label>' +
-                         '<input type="text" id="' + filter_name + '" name="' + filter_name + '"></span></div>');
-        $(content).bind('DOMNodeInserted DOMNodeRemoved', function() {
-            $('#'+filter_name).val(GM_getValue(filter_name));
-            let keyword = $('#'+filter_name).val();
-            filter(content, keyword);
-        });
-        $('#'+filter_name).on('input', function() {
-            let keyword = $('#'+filter_name).val();
-            GM_setValue(filter_name, keyword);
-            filter(content, keyword);
-        });
+    if ($(box).find('#'+filter_name).size() > 0) {
+        $('#'+filter_name).val(GM_getValue(filter_name));
+        const keyword = $('#'+filter_name).val();
+        filter(content, keyword);
+
+        return;
     }
+
+    const input = $(box).find('div[class^=chat-box-input_]');
+    $(input).prepend('<div><span style="vertical-align: middle; padding: 10px;"><label for="filter" style="color: green;">Filter: </label>' +
+                     '<input type="text" id="' + filter_name + '" name="' + filter_name + '"></span></div>');
+
+    $(content).bind('DOMNodeInserted DOMNodeRemoved', function() {
+        $('#'+filter_name).val(GM_getValue(filter_name));
+        const keyword = $('#'+filter_name).val();
+        filter(content, keyword);
+    });
+
+    $('#'+filter_name).on('input', function() {
+        const keyword = $('#'+filter_name).val();
+        GM_setValue(filter_name, keyword);
+        filter(content, keyword);
+    });
 }
 
 (function() {
     'use strict';
 
     // Your code here...
-    let boxes = $('#chatRoot').find('div[class^=chat-box_]');
-    $(boxes).each(function() {
-        $(this).bind('DOMNodeInserted', function() {
-            $(this).ready(addTradeFilter($(this)));
+    const chats = ['global', 'trade', 'faction', 'travel'];
+    chats.forEach(function(chat) {
+        const box = $('#chatRoot').find('div[class^=chat-box_][class*='+chat+'_]');
+        $(box).bind('DOMNodeInserted', function() {
+            $(box).ready(addChatFilter(box, chat));
         });
     });
 })();
