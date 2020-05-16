c86107862aa28b8d5c353c42cf8588607196e886
Igor <f2404@yandex.ru>
2020-05-16 12:42:52 -0400

    Remove dependency on DKK's lib

--- shares_hide_blocks.user.js
+++ shares_hide_blocks.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Torn: Stocks: Hide blocks
 // @namespace    lugburz.stocks.hide_blocks
-// @version      0.1.3
+// @version      0.1.4
 // @description  Allows to hide certain stock blocks from the list.
 // @author       Lugburz
 // @match        https://www.torn.com/stockexchange.php?step=portfolio
 // @match        https://www.torn.com/laptop.php*
-// @require      https://greasyfork.org/scripts/390917-dkk-torn-utilities/code/DKK%20Torn%20Utilities.js?version=744690
+// @require      https://github.com/f2404/torn-userscripts/raw/master/lib/lugburz_lib.js
+// @updateURL    https://github.com/f2404/torn-userscripts/raw/master/shares_hide_blocks.user.js
 // @grant        GM_setValue
 // @grant        GM_getValue
 // ==/UserScript==
@@ -30,13 +31,13 @@ function showHideBlocks() {
 
         let count = $(this).find('ul.item > li.info > div.b-price-wrap > div.first-row').html();
         count = count.replace('<span class="bold">Shares:</span>', '').trim();
-        let name = $(this).attr('data-stock');
-        let blockId = $(this).attr('id');
-        let liId = 'show-' + blockId;
+        const name = $(this).attr('data-stock');
+        const blockId = $(this).attr('id');
+        const liId = 'show-' + blockId;
 
         $(this).find('ul.item > li.logo').append('<a href="#" id="hideBtn" class="t-blue h t-hide" role="button" style="position: absolute; top: 5px; right: 5px;">Hide</a>');
 
-        let html = '<li id="' + liId + '" class="info"><div class="qualify-wrap" style="height: 22px; line-height: 22px; padding: 0 7px;">The <b>' +
+        const html = '<li id="' + liId + '" class="info"><div class="qualify-wrap" style="height: 22px; line-height: 22px; padding: 0 7px;">The <b>' +
             name.toUpperCase() + '</b> block (<b>' + count + '</b> shares) is hidden <span id="showBtn" class="action">' +
             '<a href="#" class="t-blue h t-hide" role="button">Show</a></span></div><div class="delimiter-wrap"></div></li>';
         $('ul.stock-cont').append(html);
@@ -63,11 +64,10 @@ function showHideBlocks() {
     'use strict';
 
     // Your code here...
-    ajax((page, json, uri) => {
+    ajax((page) => {
         if (page != "stockexchange") return;
         $('ul.stock-cont').ready(showHideBlocks);
     });
 
     $('ul.stock-cont').ready(showHideBlocks);
 })();
-
