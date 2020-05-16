0a0c1a62fe9e69b664f84fbfa3d51ac5e93dd6ec
Igor <f2404@yandex.ru>
2020-05-16 12:47:39 -0400

    Remove dependency on DKK's lib

--- racing_profiles.user.js
+++ racing_profiles.user.js
@@ -1,21 +1,23 @@
 // ==UserScript==
 // @name         Torn: Racing: Links to profiles
 // @namespace    lugburz.racing.profiles
-// @version      0.1
+// @version      0.1.1
 // @description  Adds links to player profiles to their names in the racing list.
 // @author       Lugburz
 // @match        https://www.torn.com/loader.php?sid=racing*
-// @require      https://greasyfork.org/scripts/390917-dkk-torn-utilities/code/DKK%20Torn%20Utilities.js?version=744690
+// @require      https://github.com/f2404/torn-userscripts/raw/master/lib/lugburz_lib.js
+// @updateURL    https://github.com/f2404/torn-userscripts/raw/master/racing_profiles.user.js
+// @grant        none
 // ==/UserScript==
 
 function addLinks() {
-    var names = $("ul.overview").find("li.name");
+    const names = $("ul.overview").find("li.name");
     names.each(function() {
-        var parent = $(this).parent().parent()
+        const parent = $(this).parent().parent()
         if (parent.attr("id").startsWith("lbr-")) {
-            var username = $(this).text();
-            var user_id = parent.attr("id").replace("lbr-", "");
-            $(this).html("<a href=/profiles.php?XID="+user_id+">"+username+"</a>");
+            const username = $(this).text();
+            const user_id = parent.attr("id").replace("lbr-", "");
+            $(this).html("<a href=/profiles.php?XID=" + user_id + ">" + username + "</a>");
         }
     });
 }
@@ -24,8 +26,8 @@ function addLinks() {
     'use strict';
 
     // Your code here...
-    ajax((page, json, uri) => {
-        if (page != "loader" || !json) return;
+    ajax((page) => {
+        if (page != "loader") return;
         $("ul.overview").ready(addLinks);
     });
 
