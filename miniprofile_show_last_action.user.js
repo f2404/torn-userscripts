d838868f51b0ace70f1736a7647f2c35018a7d10
Igor <f2404@yandex.ru>
2020-05-28 16:06:41 -0400

    Add Torn: Miniprofile: Show last action

---
+++ miniprofile_show_last_action.user.js
@@ -0,0 +1,74 @@
+// ==UserScript==
+// @name         Torn: Miniprofile: Show last action
+// @namespace    lugburz.miniprofile.show_last_action
+// @version      0.1
+// @description  Show last action in miniprofile.
+// @author       Lugburz
+// @match        https://www.torn.com/*
+// @grant        GM_addStyle
+// @grant        unsafeWindow
+// ==/UserScript==
+
+function secondsToDhms(seconds) {
+    seconds = Number(seconds);
+    var d = Math.floor(seconds / (3600*24));
+    var h = Math.floor(seconds % (3600*24) / 3600);
+    var m = Math.floor(seconds % 3600 / 60);
+    var s = Math.floor(seconds % 60);
+
+    var dDisplay = d > 0 ? d + "d " : "";
+    var hDisplay = d > 0 || h > 0 ? h + "h " : "";
+    var mDisplay = d > 0 || h > 0 || m > 0 ? m + "m " : "";
+    var sDisplay = s > 0 ? s + "s" : "";
+    return dDisplay + hDisplay + mDisplay + sDisplay;
+}
+
+// intercept miniprofile fetch() responses
+const constantMock = unsafeWindow.fetch;
+unsafeWindow.fetch = function() {
+    return new Promise((resolve, reject) => {
+        constantMock.apply(this, arguments).then((response) => {
+            if (response.url.indexOf('/profiles.php?step=getUserNameContextMenu') > -1) {
+                const response2 = response.clone();
+                response2.text().then(function (text) {
+                    try {
+                        const json = JSON.parse(text);
+                        console.log('userID=' + json.user.userID + ' lastAction=' + json.user.lastAction.seconds);
+                        $('#miniProfileLastAction').text(secondsToDhms(json.user.lastAction.seconds) + ' ago');
+                    } catch (e) {
+                        console.log(e);
+                    }
+                });
+            }
+            resolve(response);
+        });
+    });
+}
+
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        const newNodes = mutation.addedNodes;
+        if (newNodes) {
+            $(newNodes).each(function() {
+                if ($(this).attr('id') && $(this).attr('id') == 'profile-mini-root') {
+                    observer.disconnect();
+                    observer.observe($('#profile-mini-root').get(0), { subtree: true, childList: true });
+                } else if ($(this).attr('class') && $(this).attr('class').indexOf('-profile-mini-_userProfileWrapper___') > -1) {
+                    let id = $(this).find('div[class^=-profile-mini-_userWrap__]').find('a').attr('href');
+                    if (id) {
+                        id = id.replace(/\/?profiles.php\?XID=/, '');
+                        $(this).append('<div style="color: #555; font-size: 12px; line-height: 20px;"><b>Last action:</b> <span id="miniProfileLastAction"></span></div>');
+                    }
+                }
+            });
+        }
+    });
+});
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    const wrapper = $('#body');
+    observer.observe($(wrapper).get(0), { subtree: true, childList: true });
+})();
