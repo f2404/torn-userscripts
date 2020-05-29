7e4cac5f795e2997e127f0ad5714b32acf5117a5
Scott Curtis <scott.curtis@hotmail.com>
2020-05-29 19:55:44 +1200

    Allowing removal of NPC + bugfix

--- npc_profile_loot_timer.user.js
+++ npc_profile_loot_timer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn: Loot timer on NPC profile
 // @namespace    lugburz.show_timer_on_npc_profile
-// @version      0.2.5
+// @version      0.2.6
 // @description  Add a countdown timer to desired loot level on the NPC profile page as well as on the sidebar and the topbar (optionally).
 // @author       Lugburz
 // @match        https://www.torn.com/*
@@ -25,6 +25,8 @@ const TOPBAR_TIMERS = true;
 // Whether or not to change the timer color when it's close to running out
 const CHANGE_COLOR = true;
 
+// The NPC's to watch. Remove any that you don't want
+const NPCS = { 'Duke': 4, 'Scrooge': 10, 'Leslie': 15, 'Jimmy': 19 };
 
 
 GM_addStyle(`
@@ -55,31 +57,30 @@ GM_addStyle(`
   transition: color .2s ease;
 }`);
 
-const IDS = [4, 10, 15, 19]; // Duke, Scrooge, Leslie, Jimmy
-const NAMES = ['Duke', 'Scrooge', 'Leslie', 'Jimmy'];
 const ROMAN = ['I', 'II', 'III', 'IV', 'V'];
+const LOGGING_ENABLED = false;
 
 const yata_api = async () => {
-  return new Promise((resolve, reject) => {
-    GM_xmlhttpRequest ( {
-      method: "GET",
-      url: 'https://yata.alwaysdata.net/loot/timings/',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      onload: (response) => {
-          try {
-            const resjson = JSON.parse(response.responseText)
-            resolve(resjson)
-          } catch(err) {
-            reject(err)
-          }
-      },
-      onerror: (err) => {
-        reject(err)
-      }
+    return new Promise((resolve, reject) => {
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: 'https://yata.alwaysdata.net/loot/timings/',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            onload: (response) => {
+                try {
+                    const resjson = JSON.parse(response.responseText)
+                    resolve(resjson)
+                } catch (err) {
+                    reject(err)
+                }
+            },
+            onerror: (err) => {
+                reject(err)
+            }
+        })
     })
-  })
 }
 
 function isCachedDataValid(id = '') {
@@ -106,14 +107,14 @@ function isCachedDataValid(id = '') {
 
 async function getTimings(id) {
     if (!isCachedDataValid(id)) {
-        console.log('Calling YATA id=' + id);
+        log('Calling YATA id=' + id);
         const data = await yata_api();
         GM_setValue('cached_data', JSON.stringify(data));
     }
 
     const cached_data = JSON.parse(GM_getValue('cached_data'));
     if (cached_data.error) {
-        console.log('YATA API error');
+        log('YATA API error');
         return -1;
     }
     // no data on the id
@@ -126,35 +127,23 @@ async function getTimings(id) {
 
 async function getAllTimings() {
     if (!isCachedDataValid()) {
-        console.log('Calling YATA');
+        log('Calling YATA');
         const data = await yata_api();
         GM_setValue('cached_data', JSON.stringify(data));
     }
 
     const cached_data = JSON.parse(GM_getValue('cached_data'));
     if (cached_data.error) {
-        console.log('YATA API error');
+        console.error('YATA API error');
         return '';
     }
     return cached_data;
 }
 
-function hideSidebarTimers(hide) {
-    if (hide) {
-        for (let i = 0; i < IDS.length; i++) {
-            const id = IDS[i];
-            const pId = '#npcTimer' + id;
-            $(pId).hide();
-        }
-        $('#showHideTimers').text('[show]');
-    } else {
-        for (let i = 0; i < IDS.length; i++) {
-            const id = IDS[i];
-            const pId = '#npcTimer' + id;
-            if (!$(pId).attr('notavail')) $(pId).show();
-        }
-        $('#showHideTimers').text('[hide]');
-    }
+function hideSidebarTimers(hide, yataData) {
+    log(yataData)
+    Object.values(NPCS).forEach(id => (hide || yataData[id] === undefined) ? $(`#npcTimer${id}`).hide() : $(`#npcTimer${id}`).show())
+    $('#showHideTimers').text(`[${hide ? 'show' : 'hide'}]`);
 }
 
 function maybeChangeColors(span, left) {
@@ -178,7 +167,7 @@ function process(ts) {
     // ts is s, Date is ms
     const due = new Date(ts * 1000);
 
-    let x = setInterval(function() {
+    let x = setInterval(function () {
         const now = new Date().getTime();
         const left = due - now;
         if (left < 0) {
@@ -190,38 +179,39 @@ function process(ts) {
         const span = $('#profileroot').find('div.profile-status').find('div.profile-container').find('div.description').find('span.sub-desc');
         let html = $(span).html();
         const n = html.indexOf('(');
-        html = html.substring(0, n != -1 ? n-1 : html.length);
-        $(span).html(html + " (Till loot level " + ROMAN[LOOT_LEVEL-1] + ": " + formatTimeSecWithLetters(left) + ")");
+        html = html.substring(0, n != -1 ? n - 1 : html.length);
+        $(span).html(html + " (Till loot level " + ROMAN[LOOT_LEVEL - 1] + ": " + formatTimeSecWithLetters(left) + ")");
     }, 1000);
 }
 
 function addNpcTimers(data) {
-    if (!data) {
+    if (!data)
         return;
-    }
 
+    log('Adding NPC Timers for:')
+    log(NPCS)
     if (SIDEBAR_TIMERS && $('#sidebarNpcTimers').size() < 1) {
         let div = '<hr class="delimiter___neME6"><div id="sidebarNpcTimers"><span style="font-weight: 700;">NPC Timers</span><a id="showHideTimers" class="show-hide">[hide]</a>';
-        for (let i = 0; i < IDS.length; i++) {
-            div += '<p style="line-height: 20px; text-decoration: none;" id="npcTimer' + IDS[i] + '"><a class="t-blue href desc" style="display:inline-block; width: 52px;" href="/loader.php?sid=attack&user2ID=' +
-                   IDS[i] + '">' + NAMES[i] + ': </a><span></span></p>';
-        }
+        Object.keys(NPCS).forEach(name => {
+            div += '<p style="line-height: 20px; text-decoration: none;" id="npcTimer' + NPCS[name] + '"><a class="t-blue href desc" style="display:inline-block; width: 52px;" href="/loader.php?sid=attack&user2ID=' +
+                NPCS[name] + '">' + name + ': </a><span></span></p>';
+        });
         div += '</div>';
         $('#sidebar').find('div[class^=toggle-content__]').find('div[class^=content___]').append(div);
         //$(div).insertBefore($('#sidebar').find('h2[class^=header__]').eq(1)); // second header
-        $('#showHideTimers').on('click', function() {
+        $('#showHideTimers').on('click', function () {
             const hide = $('#showHideTimers').text() == '[hide]';
             GM_setValue('hideSidebarTimers', hide);
-            hideSidebarTimers(hide);
+            hideSidebarTimers(hide, data);
         });
     }
 
     if (TOPBAR_TIMERS && $('#topbarNpcTimers').size() < 1) {
         let div = '<div id="topbarNpcTimers" style="text-align: center;"><span style="font-weight: 700;">NPC Timers:</span>';
-        for (let i = 0; i < IDS.length; i++) {
-            div += '<span style="line-height: 20px; text-decoration: none;" id="npcTimerTop' + IDS[i] + '"><a class="t-blue href desc" style="display:inline-block; width: 52px;" href="/loader.php?sid=attack&user2ID=' +
-                   IDS[i] + '">' + NAMES[i] + ': </a><span></span></span>';
-        }
+        Object.keys(NPCS).forEach(name => {
+            div += '<span style="line-height: 20px; text-decoration: none;" id="npcTimerTop' + NPCS[name] + '"><a class="t-blue href desc" style="display:inline-block; width: 52px;" href="/loader.php?sid=attack&user2ID=' +
+                NPCS[name] + '">' + name + ': </a><span></span></span>';
+        });
         div += '</div>';
         if ($('div.header-wrapper-bottom').find('div.container').size() > 0) {
             // announcement
@@ -233,20 +223,19 @@ function addNpcTimers(data) {
 
     if (SIDEBAR_TIMERS) {
         const hide = GM_getValue('hideSidebarTimers');
-        hideSidebarTimers(hide);
+        hideSidebarTimers(hide, data);
     }
 
-    for (let i = 0; i < IDS.length; i++) {
-        const id = IDS[i];
+    Object.keys(NPCS).forEach(name => {
+        const id = NPCS[name]
         const pId = '#npcTimer' + id;
         const spanId = '#npcTimerTop' + id;
         if (data[id]) {
-            const name = NAMES[i];
             const ts = data[id].timings[LOOT_LEVEL].ts;
 
             // ts is s, Date is ms
             const due = new Date(ts * 1000);
-            let x = setInterval(function() {
+            let x = setInterval(function () {
                 const now = new Date().getTime();
                 const left = due - now;
                 if (left < 0) {
@@ -274,16 +263,16 @@ function addNpcTimers(data) {
             $(spanId).attr('notavail', 1);
             $(spanId).hide();
         }
-    }
+    })
 }
 
-(function() {
+(function () {
     'use strict';
 
     // Your code here...
     if ($(location).attr('href').includes('profiles.php')) {
         const profileId = RegExp(/XID=(\d+)/).exec($(location).attr('href'))[1];
-        if (IDS.includes(Number(profileId))) {
+        if (Object.values(NPCS).includes(Number(profileId))) {
             getTimings(profileId).then(ts => process(ts));
         }
     }
@@ -291,3 +280,8 @@ function addNpcTimers(data) {
         getAllTimings().then(data => addNpcTimers(data));
     }
 })();
+
+function log(data) {
+    if (LOGGING_ENABLED)
+        console.log(data)
+}
