657d4246f6d3d2c1054a1974abfa10bc683706b8
Igor <f2404@yandex.ru>
2021-03-30 14:23:07 -0400

    Add Easter Bunny

--- npc_profile_loot_timer.user.js
+++ npc_profile_loot_timer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn: Loot timer on NPC profile
 // @namespace    lugburz.show_timer_on_npc_profile
-// @version      0.2.22
+// @version      0.2.23
 // @description  Add a countdown timer to desired loot level on the NPC profile page as well as on the sidebar and the topbar (optionally).
 // @author       Lugburz
 // @match        https://www.torn.com/*
@@ -30,7 +30,8 @@ const NPCS = {
     'Scrooge': { id: 10, loot_level: 4 },
     'Leslie': { id: 15, loot_level: 4 },
     'Jimmy': { id: 19, loot_level: 4 },
-    'Fernando': { id: 20, loot_level: 4 }
+    'Fernando': { id: 20, loot_level: 4 },
+    'Easter Bunny': { id: 17, loot_level: 4 }
 };
 
 
