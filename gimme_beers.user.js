8f1fe964394f1d57b740ebf225bb55f5ec6cc5f8
Igor <f2404@yandex.ru>
2021-10-21 14:44:33 -0400

    Create gimme_beers.user.js

---
+++ gimme_beers.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Torn: Gimme beers
+// @namespace    lugburz.gimme_beers
+// @version      0.1
+// @description  Gimme beers!
+// @author       Lugburz
+// @match        https://www.torn.com/shops.php*
+// @grant        none
+// ==/UserScript==
+
+function addButton() {
+    if ($('div.content-title > h4').size() > 0 && $('#buyBeerBtn').size() < 1) {
+        const button = `<button id="buyBeerBtn" style="color: var(--default-blue-color); cursor: pointer; margin-right: 0;">Gimme beers!</button>
+                        <span id="buyBeerResult" style="font-size: 12px; font-weight: 100;"></span>`;
+        $('div.content-title > h4').append(button);
+        $('#buyBeerBtn').on('click', async () => {
+            $('#buyBeerResult').text('');
+            await getAction({
+                type: 'post',
+                action: 'shops.php',
+                data: {
+                    step: 'buyShopItem',
+                    ID: 180,
+                    amount: 100
+                },
+                success: (str) => {
+                    try {
+                        const msg = JSON.parse(str);
+                        $('#buyBeerResult').html(msg.text).css('color', msg.success ? 'green' : 'red');
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+            });
+        });
+    }
+};
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    addButton();
+})();
